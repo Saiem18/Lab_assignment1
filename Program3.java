5cac275445bb6eb43f7a95c03ad405563302671f
Saiem18 <139449934+Saiem18@users.noreply.github.com>
2023-07-19 00:17:54 +0530

    Update Program3.java

--- Program3.java
+++ Program3.java
@@ -43,4 +43,29 @@ public class Greatestof_3num {
 	}
 
 }
+
+
+*/
+
+
+/*//Even or odd
+package Saiem;
+import java.util.*;
+public class Even_odd {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number: ");
+		int a=sc.nextInt();
+		if(a%2==0) {
+			System.out.println("The number is even");
+		}else {
+			System.out.println("The number is odd");
+		}
+		
+
+	}
+
+}
+
 */
